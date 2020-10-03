e76c87f3786f05628ba42d271548cdcc9d5383f2
FelixWessel <felix.wessel@googlemail.com>
2020-10-03 18:08:22 +0200

    Creating the main game loop and setting up a draw function

--- SimpleSnakeGame.py
+++ SimpleSnakeGame.py
@@ -4,12 +4,28 @@ from random import randint
 
 pygame.init()
 
+run = True
+
 screen_width = 600
 screen_height = 600
 
+square_x_pos = 60
+square_y_pos = 60
+square_dimension_w_h = 20
+
 game_window = pygame.display.set_mode((screen_width, screen_height))
 
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
+    pygame.draw.rect(game_window, (255, 0, 0), (square_x_pos, square_y_pos, square_dimension_w_h, square_dimension_w_h))
 
+    pygame.display.update()
+    window.fill((0, 0, 0))
+    
+pygame.quit()
 
 
