2e386a5e0920ad5932afd92f960a03a1e862ff9e
FelixWessel <felix.wessel@googlemail.com>
2020-10-04 18:12:01 +0200

    Adding some helpful comments

--- SimpleSnakeGame.py
+++ SimpleSnakeGame.py
@@ -1,21 +1,26 @@
+# This is a work in progress - not finished and not working yet
+
 import pygame
 import sys
 from random import randint
 
 pygame.init()
 
-run = True
+#Definition of variables for the game
+run = True                  # As long as run equals true the game runs
+screen_width = 600          # Setting the width of the game window
+screen_height = 600         # Setting the height of the game window
+square_x_pos = 60           # The initial x-position of the snake head at start of the game
+square_y_pos = 60           # The initial y-position of the snake head at start of the game
+square_dimension_w_h = 20   # Setting the hight and width of snake's building blocks
 
-screen_width = 600
-screen_height = 600
 
-square_x_pos = 60
-square_y_pos = 60
-square_dimension_w_h = 20
-
+# Defining our game window
 game_window = pygame.display.set_mode((screen_width, screen_height))
 
+# This is the main loop of our SimpleSnakeGame
 while run:
+    # Here we check for inputs and exit the game if we close the game window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
